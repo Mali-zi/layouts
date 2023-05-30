4a3e42fcb7305b89e55293de2229d235f78011ed
Mali-zi <mne310872@yandex.ru>
2023-05-30 22:25:03 +0300

    docs: add screenshots in img

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,11 +1,26 @@
+import Button from 'react-bootstrap/Button';
+
+
 function ShopItem({ name, price, color, img }) {
   return (
-    <div>
-      <img src={img} alt=" "></img>
-      <span>{name}</span>
-      <span>{color}</span>
-      <span>{price}</span>
-      <button type="button">Add to cart</button>
+    <div className="container-md text-center">
+      <div className="row justify-content-md-center align-items-center list-row">
+        <div className="col">
+          <img className="img-fluid item-img" src={img} alt={name}></img>
+        </div>
+        <div className="col">
+          <span className='item-title'>{name}</span>
+        </div>
+        <div className="col">
+          <span className='card-subtitle'>{color}</span>
+        </div>
+        <div className="col">
+          <span className='card-price'>${price}</span>
+        </div>
+        <div className="col">
+          <Button variant="outline-danger" className='item-btn'>Add to cart</Button>
+        </div>
+      </div>
     </div>
   )
 };
